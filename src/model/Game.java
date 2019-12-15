5dccc6869b408f1a0897ae6aa1ab885ef62762d5
David <56723830+dmholtz@users.noreply.github.com>
2019-12-15 09:46:29 +0100

    repo setup

---
+++ src/model/Game.java
@@ -0,0 +1,11 @@
+package model;
+
+public abstract class Game {
+	
+	protected Player player1;
+	protected Player player2;
+	
+	
+	
+
+}
---
+++ src/model/Player.java
@@ -0,0 +1,10 @@
+package model;
+import java.awt.Color;
+
+public abstract class Player {
+	
+	private String name;
+	private Color color;
+	private Symbol symbol;
+
+}
